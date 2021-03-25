d21ac9bde5757b4be30aa921aa432ce696e5b90e
Sayansree <sayansreeparia@gmail.com>
2021-03-26 01:30:58 +0530

    update declaration
    Signed-off-by: Sayansree <sayansreeparia@gmail.com>

--- PID.cpp
+++ PID.cpp
@@ -8,9 +8,19 @@ PID::~PID(){
 }
 double PID::update(double error){
     I=trim(I+error,Imin,Imax);
+    if(firstRun){
+        prevError=error;
+        firstRun=false;
+    }
     double p= Kp*error;
     double i= Ki*I;
     double d= Kd*(error-prevError);
     double pid = trim(p+i+d,PIDmin,PIDmax);
+    prevError=error;
     return pid;
+}
+void PID::reset(){
+    I=0;
+    prevError=0;
+    firstRun=true;
 }
--- PID.h
+++ PID.h
@@ -5,13 +5,14 @@ class PID{
         PID();
         ~PID();
         double update(double);
+        void reset();
 
     private:
         double Kp,Ki,Kd;
         double I, Imin, Imax;
         double prevError;
         double PIDmin, PIDmax;
-
+        bool firstRun;
         double trim(double, double, double);
 
 };
