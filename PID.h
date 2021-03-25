7e30f88b9e00d5db68d84b13a931f51ae26f62fd
Sayansree <sayansreeparia@gmail.com>
2021-03-26 01:22:00 +0530

    added class parameters
    Signed-off-by: Sayansree <sayansreeparia@gmail.com>

--- PID.h
+++ PID.h
@@ -4,11 +4,15 @@ class PID{
     public:
         PID();
         ~PID();
-        
+        double update(double);
 
     private:
         double Kp,Ki,Kd;
-        
+        double I, Imin, Imax;
+        double prevError;
+        double PIDmin, PIDmax;
+
+        double trim(double, double, double);
 
 };
 #endif
